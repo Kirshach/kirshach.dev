a86fabfb33743dc03517b515c568d6db2765acb5
Dmitrii Kirshanov <kirshach@mail.ru>
2024-01-16 01:02:01 +0400

    feat: add Solid.js support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import solidJs from "@astrojs/solid-js";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://kirshach.dev",
+  integrations: [solidJs()]
 });
