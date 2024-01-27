74f440caca8094a42056cf24c6108fbf0c5806b7
Dmitrii Kirshanov <kirshach@mail.ru>
2024-01-27 16:21:13 +0400

    feat: move Astro config to TS

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://kirshach.dev",
   prefetch: true,
